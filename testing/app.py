4d90225907868ea48c76c00490cab5b27fdb9479
Drakon <75917187+Dr4k0n-D3v@users.noreply.github.com>
2023-02-16 11:45:26 -0500

    Add files via upload

---
+++ testing/app.py
@@ -0,0 +1,98 @@
+from flask import Flask, request, jsonify
+import os, json, logger
+from flask_cors import CORS
+
+tof = {
+    'true':'True',
+    'false':'False'
+}
+
+app = Flask(__name__)
+CORS(app)
+
+@app.route("/api/log", methods=["POST"])
+def login():
+    email = request.json['email']
+    password = request.json['password']
+
+    with open('data/login.json') as f:
+        file = json.load(f)
+    log = False
+    for obj in file:
+        if obj['email'] == email and obj['passwd'] == password:
+            log = True
+            break
+    if log:
+        response = {
+            'status': 'success',
+            'message': 'Login successful'
+        }
+        return json.dumps(response), 200
+    else:
+        response = {
+            'status': 'error',
+            'message': 'Incorrect email or password'
+        }
+        return json.dumps(response), 401
+
+@app.route('/api/signu', methods=["POST"])
+def signup():
+    user = request.form.get('user')
+    email = request.form.get('email')
+    password = request.form.get('password')
+    with open('data/login.json') as f:
+        file = json.load(f)
+    var = True
+    for obj in file:
+        if obj['email'] == email or not var:
+            var = False
+            break
+    if var:
+        data = {f'user':{
+                'name':f'{user}',
+                'email':f'{email}',
+                'password':f'{password}'
+            }
+        }
+        file.update(data)
+        with open('data/login.json', "w") as f:
+            f.write(json.dumps(file))
+            f.close()
+        response = {
+            'status': 'success',
+            'message': 'Sign up successful'
+        }
+        return jsonify(response), 201
+    else:
+        response = {
+            'status': 'failure',
+            'message': 'Sign up failed'
+        }
+        return jsonify(response), 400
+
+@app.route('/api/post', methods=["POST"])
+def post():
+    posts = 'data/posts'
+    response = {'success': False}
+    try:
+        title = request.form.get('title')
+        desc = request.form.get('desc')
+        file = 'ohno'
+        try:
+            file = request.files['file']
+        except:
+            pass
+        path = os.path.join(posts, title)
+        os.mkdir(path)
+        with open(f"{path}/desc.txt", "w") as f:
+            f.write(desc)
+            f.close()
+        file.save(os.path.join(path, file.filename))
+        response['success'] = True
+    except:
+        pass
+    return jsonify(response)
+
+
+if __name__ == "__main__":
+    app.run(port=8080, debug=True)
